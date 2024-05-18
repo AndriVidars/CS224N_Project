b5164828cca99255f09fdbc311ad1b80fc1002e1
AndriVidars <andriv@stanford.edu>
2024-05-17 21:57:12 -0700

    add command line args for augmentation.py

--- augmentation.py
+++ augmentation.py
@@ -5,8 +5,12 @@ from transformers import T5ForConditionalGeneration, T5Tokenizer
 from tqdm import tqdm
 import pickle
 import numpy as np
+import argparse
+import string
+import pandas as pd
 
 model_name = "google/flan-t5-xxl"
+
 class DataAugmentator():
     def __init__(self, prompt_base):
         self.prompt_base = prompt_base
@@ -20,6 +24,7 @@ class DataAugmentator():
         input_ids = self.tokenizer.encode(input_text, return_tensors="pt").to(self.device)
         input_length = input_ids.shape[1] # input length in number of tokens
         max_length = int(1.5 * input_length + 10)
+        # maybe add min_length, though that is likely to cause memory issues
         num_beam_groups = 10  # Set this to a value larger than 1 for diversity
         with torch.no_grad():
             outputs = self.model.generate(
@@ -42,15 +47,18 @@ class DataAugmentator():
     def augment_dataset(self, text, labels, rep=5):
         # Augment training data
         # Get rep paraphrases for example in the dataset
-
         data_out = []
         for i, s in tqdm(enumerate(text)):
             paraphrases = self.generate_paraphrases(s, rep)
             data_out.append((s, labels[i], paraphrases))
-        
         return data_out
 
-def read_sentiment_train(filename):
+def generate_custom_id(length=24):
+    characters = string.ascii_lowercase + string.digits
+    return ''.join(random.choices(characters, k=length))
+
+# format for movie datasets ids-cfimbd and ids-sst is the same
+def read_movie_train(filename):
     data = []
     with open(filename, 'r') as fp:
         for record in csv.DictReader(fp,delimiter = '\t'):
@@ -60,21 +68,58 @@ def read_sentiment_train(filename):
             data.append((sent, label,sent_id))
     return data
 
+def write_movie_aug_data_file(aug_data, source_data_file, num_samples, num_reps):
+    # format for movie datasets, ids-cfimbd and ids-sst
+    data = {
+        'id': [],
+        'sentence':[],
+        'sentiment':[]
+    }
+
+    for x in aug_data:
+        sentiment = x[1]
+        for s in x[2]:
+            data['id'].append(generate_custom_id())
+            data['sentence'].append(s)
+            data['sentiment'].append(sentiment)
+    
+    source_df = pd.read_csv(f'data/{source_data_file}.csv' , delimiter='\t')
+    if 'Unnamed: 0' in source_df.columns:
+        source_df = source_df.drop(columns=['Unnamed: 0'])
+
+    aug_df = pd.DataFrame(data)
+    merged_df = pd.concat([source_df, aug_df], ignore_index=True)
+    merged_df.to_csv(f'data/{source_data_file}-aug_{num_samples}_{num_reps}.csv', sep='\t', index_label='number', index=True)
+
 if __name__ == "__main__":
-    file = 'data/ids-sst-train.csv'
-    train_data = read_sentiment_train(file)
-    idxs = np.random.randint(0, len(train_data)-1, 1750) # 1750 samples
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file', type=str, required=True, help='Name of training file, not including extension')
+    parser.add_argument('--num_samples', type=int, required=True, help='The number of samples')
+    parser.add_argument('--num_reps', type=int, required=True, help='The number of reps per sample')
+    args = parser.parse_args() # TODO add parameter for file type, movie review/sentence similarity
+
+    source_file = args.file
+    num_samples = args.num_samples
+    num_reps = args.num_reps
+
+    #file = 'data/ids-sst-train.csv'
+    train_data = read_movie_train(f'data/{source_file}.csv')
+    idxs = np.random.randint(0, len(train_data)-1, num_samples) # 1750 samples
     sample_data = [train_data[i] for i in idxs]
     sents = [x[0] for x in sample_data]
     labels = [x[1] for x in sample_data]
     
     data_aug = DataAugmentator("paraphrase the following excerpt from a movie review:")
-    data_augmented = data_aug.augment_dataset(sents, labels, 5)
-
-    with open('ids-sst-train-aug.pkl', 'wb') as f:
+    data_augmented = data_aug.augment_dataset(sents, labels, num_reps)
+    
+    # dump raw augmentation output for analysis
+    with open(f'data/{source_file}-aug_{num_samples}_{num_reps}.pkl', 'wb') as f:
         pickle.dump(data_augmented, f)
-
-    # run the following to get a sense of the functionality
+    
+    # save to csv
+    write_movie_aug_data_file(data_augmented, source_file, num_samples, num_reps)
+    
+    # old code,  run the following to get a sense of the functionality
     """
     for sent_id in [random.randint(0, len(sents) - 1) for _ in range(25)]:
         s = sents[sent_id]
@@ -85,5 +130,3 @@ if __name__ == "__main__":
             print(p, '\n')
         print("-"*40, '\n')
     """
-
-        
